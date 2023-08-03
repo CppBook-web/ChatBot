ea9f81b90e595e97246259e4f7aa481e8694473b
ProgBot <32187545+CppBook-web@users.noreply.github.com>
2023-08-03 14:46:10 +0300

    Update learner.cpp

--- ChatBot/learner.cpp
+++ ChatBot/learner.cpp
@@ -5,8 +5,7 @@
 
 /*
 Следующая функция будет искать переданную фразу в файле памяти.
-Если есть совпадение, будет выведен сопроводительный ответ, 
-сохраненный под начальной фразой.
+Если есть совпадение, будет выведен ответ, сохраненный под начальной фразой.
 Если ответ не найден, Learner повторит фразу и предложит
 пользователю ввести ответ. Этот ответ будет сохранен в файле памяти
 вместе с начальной фазой.
@@ -14,10 +13,10 @@
 
 void Learner::respond(const std::string phrase){
     std::fstream memory;
-    memory.open("memory.txt", std::ios::in);      // открыть файл памяти
-    if (memory.fail()) {                          // создать файл если его не существует
+    memory.open("memory.txt", std::ios::in);      // Открыть файл памяти
+    if (memory.fail()) {                          // Создать файл если его не существует
         memory.open("memory.txt", std::ios::out);
-        memory.close();
+        memory.close();                           // Закрыть файл
     }
     
     while( !memory.eof() ) {          // Поиск до конца файла
@@ -30,19 +29,19 @@ void Learner::respond(const std::string phrase){
             return answer(response);     // Получаем ответ      
         }
     }
-    memory.close();                              // Фраза не найдена в памяти. Закрыть файл
+    memory.close();                              // Фраза не найдена в памяти
     
     memory.open("memory.txt",
                 std::ios::out | std::ios::app);  // Открыть файл и добавить в конец файла
     memory << phrase << std::endl;               // Записать полученную фразу в память
 
-    answer(phrase);                                 // Повторить фразу, введенную пользователем
+    answer(phrase);                              // Повторить фразу, введенную пользователем
     std::string userResponse;
     getline(std::cin, userResponse);             // Получить подходящий ответ
     memory << userResponse << std::endl;         // Записать подходящий ответ в память
-    memory.close();                              // Закрыть файл
+    memory.close();                              
 }
 
-void Learner::answer(const std::string phrase) {    // Вывод ответа Learner
+void Learner::answer(const std::string phrase) {  // Вывод ответа Learner
     std::cout << "- " << phrase << std::endl;
 }
